2d8b11ae476a3344d8f5ba68d4a1a1cb089cc8ab
7oiden <7oiden@gmail.com>
2021-12-10 13:33:31 +0100

    js hero added

--- js/index.js
+++ js/index.js
@@ -2,37 +2,41 @@ import { mobileToggler } from "./components/dropdownTogglers.js";
 import { adminToggler } from "./components/dropdownTogglers.js";
 import adminLogin from "./components/common/adminLogin.js";
 import { validateNewsletterForm } from "./components/newsletter.js";
-import { productsUrl, homeUrl } from "./settings/api.js";
+import { productsUrl, heroUrl } from "./settings/api.js";
 import displayAlert from "./components/common/displayAlert.js";
 import { renderFeaturedProducts } from "./ui/renderFeaturedProducts.js";
 import createAdminNav from "./components/common/createAdminNav.js";
 import { basketCounter } from "./components/common/basketCounter.js";
+import { renderHero } from "./ui/renderHero.js";
 
 basketCounter();
 createAdminNav();
 adminLogin();
 
-// (async function fetch() {
-//   try {
-//     const response = await fetch(homeUrl);
-//     const result = await response.json();
 
-//     console.log(result);
-//   } catch (error) {
-//     displayAlert(
-//       "alert error",
-//       "An error has occured when trying to fetch the API",
-//       ".featured__grid"
-//     );
-//   }
-// })();
+
+(async function fetchApi() {
+  try {
+    const response = await fetch(heroUrl);
+    const result = await response.json();
+
+    // console.log(result);
+    renderHero(result);
+  } catch (error) {
+    displayAlert(
+      "alert error",
+      "An error has occured when trying to fetch the API",
+      ".featured__grid"
+    );
+  }
+})();
 
 (async function fetchApi() {
   try {
     const response = await fetch(productsUrl);
     const json = await response.json();
 
-    // console.log(json);
+    console.log(json);
 
     renderFeaturedProducts(json);
   } catch (error) {
---
+++ js/ui/renderHero.js
@@ -0,0 +1,30 @@
+const heroContainer = document.querySelector(".hero__container");
+
+export function renderHero(hero) {
+  console.log(hero);
+
+  const heroImage = hero.hero_url;
+  const heroImageSmall = hero.hero_small_url;
+
+  console.log(heroImageSmall);
+
+  heroContainer.style.background = `url(${heroImage})`;
+  
+  if (window.innerWidth <= 992) {
+    heroContainer.style.background = `url(${heroImageSmall})`;
+    heroContainer.style.backgroundSize = "cover";
+  } else if (screen.width > 992) {
+    heroContainer.style.background = `url(${heroImage})`;
+    heroContainer.style.backgroundSize = "cover";
+  }
+
+  window.onresize = function () {
+    if (window.innerWidth <= 992) {
+      heroContainer.style.background = `url(${heroImageSmall})`;
+      heroContainer.style.backgroundSize = "cover";
+    } else {
+      heroContainer.style.background = `url(${heroImage})`;
+      heroContainer.style.backgroundSize = "cover";
+    }
+  };
+}
