1696398e5c9c35a8a3021c3745a9151fffd16605
Tommy Johnsen <7oiden@gmail.com>
2022-11-18 17:29:43 +0100

    slider improved

--- js/ui/renderFeaturedProducts.js
+++ js/ui/renderFeaturedProducts.js
@@ -47,32 +47,13 @@ export function renderFeaturedProducts(products) {
   });
 
   rightArrow.addEventListener("click", function () {
-    sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
+    if (window.innerWidth < 768) {
+      sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
+    } else {
+      sectionIndex = sectionIndex < 2 ? sectionIndex + 1 : 2;
+    }
     document.querySelector(".selected").classList.remove("selected");
     indicatorParents.children[sectionIndex].classList.add("selected");
     slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
   });
-
-  // featuredProducts.forEach((product) => {
-  //   featuredContainer.innerHTML += `
-  //   <div class="card__container">
-  //     <a href="products-details.html?id=${product.id}" class="card">
-  //       <div class="card__image-container">
-  //         <img src="${product.image_url}" alt="${product.image_alt_text}"
-  //           class="card__image" />
-  //       </div>
-  //       <div class="card__body">
-  //         <div class="card__header-wrapper">
-  //           <h4 class="card__title">${product.title}</h4>
-  //           <div class="card__icon-container">
-  //             <svg aria-hidden="true" class="card__icon" viewBox="0 0 24 24">
-  //               <path fill="currentColor" d="M14.5,7.66L20.64,6.97L17,12L20.68,16.97L14.5,16.32L12.03,22L9.5,16.34L3.36,17.03L7,12L3.32,7.03L9.5,7.68L11.97,2L14.5,7.66Z" />
-  //             </svg></div>
-  //         </div>
-  //         <p class="card__price">$${product.price.toFixed(2)}</p>
-  //         <p class="card__text">${product.short_description}</p>
-  //       </div>
-  //     </a>
-  //   </div>`;
-  // });
 }
