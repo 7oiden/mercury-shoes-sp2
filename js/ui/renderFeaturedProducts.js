426bbfed8d8096090e94603e549bef04fb474565
Tommy Johnsen <7oiden@gmail.com>
2022-11-19 12:18:28 +0100

    slider fixes

--- js/ui/renderFeaturedProducts.js
+++ js/ui/renderFeaturedProducts.js
@@ -1,3 +1,5 @@
+import slider from "./slider.js";
+
 const sliderContainer = document.querySelector(".slider");
 
 export function renderFeaturedProducts(products) {
@@ -22,38 +24,5 @@ export function renderFeaturedProducts(products) {
         </div>
       </a>`;
   });
-
-  const slider = document.querySelector(".slider");
-  const leftArrow = document.querySelector("#left");
-  const rightArrow = document.querySelector("#right");
-  const indicatorParents = document.querySelector(".indicator-container");
-
-  let sectionIndex = 0;
-
-  document.querySelectorAll(".indicator").forEach(function (indicator, index) {
-    indicator.addEventListener("click", function () {
-      sectionIndex = index;
-      document.querySelector(".selected").classList.remove("selected");
-      indicator.classList.add("selected");
-      slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-    });
-  });
-
-  leftArrow.addEventListener("click", function () {
-    sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
-    document.querySelector(".selected").classList.remove("selected");
-    indicatorParents.children[sectionIndex].classList.add("selected");
-    slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-  });
-
-  rightArrow.addEventListener("click", function () {
-    if (window.innerWidth < 768) {
-      sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
-    } else {
-      sectionIndex = sectionIndex < 2 ? sectionIndex + 1 : 2;
-    }
-    document.querySelector(".selected").classList.remove("selected");
-    indicatorParents.children[sectionIndex].classList.add("selected");
-    slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-  });
+  slider()
 }
---
+++ js/ui/slider.js
@@ -0,0 +1,35 @@
+export default function slider() {
+  const slider = document.querySelector(".slider");
+  const leftArrow = document.querySelector("#left");
+  const rightArrow = document.querySelector("#right");
+  const indicatorParents = document.querySelector(".indicator-container");
+
+  let sectionIndex = 0;
+
+  document.querySelectorAll(".indicator").forEach(function (indicator, index) {
+    indicator.addEventListener("click", function () {
+      sectionIndex = index;
+      document.querySelector(".selected").classList.remove("selected");
+      indicator.classList.add("selected");
+      slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+    });
+  });
+
+  leftArrow.addEventListener("click", function () {
+    sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
+    document.querySelector(".selected").classList.remove("selected");
+    indicatorParents.children[sectionIndex].classList.add("selected");
+    slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+  });
+
+  rightArrow.addEventListener("click", function () {
+    if (window.innerWidth < 768) {
+      sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
+    } else {
+      sectionIndex = sectionIndex < 2 ? sectionIndex + 1 : 2;
+    }
+    document.querySelector(".selected").classList.remove("selected");
+    indicatorParents.children[sectionIndex].classList.add("selected");
+    slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+  });
+}
