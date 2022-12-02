563b248b219a4e542bf7fb6501af74d9a678d21b
Tommy Johnsen <7oiden@gmail.com>
2022-12-02 12:48:29 +0100

    filter func improved

--- js/components/forms/filterProducts.js
+++ js/components/forms/filterProducts.js
@@ -2,18 +2,15 @@ import { renderProducts } from "../../ui/renderProducts.js";
 
 export function filterProducts(products) {
   const filterInput = document.querySelector("#filter-select");
-  // const filterIcon = document.querySelector(".filter-icon");
-  // const filterForm = document.querySelector("#filter-form");
-
-  console.log(products);
-
-  //   filterIcon.onclick = function () {
-  //     if (filterForm.style.display === "block") {
-  //       filterForm.style.display = "none";
-  //     } else {
-  //       filterForm.style.display = "block";
-  //     }
-  //   };
+  const sortIcon = document.querySelector("#sort-icon");
+  const searchForm = document.querySelector("#search-form");
+ 
+  sortIcon.onclick = function () {
+    renderProducts(products.sort(
+      (a, b) => new Date(a.created_at) - new Date(b.created_at)
+    ));
+    searchForm.reset();
+  };
 
   filterInput.onchange = function (event) {
     const filterValue = event.target.value;
@@ -37,6 +34,12 @@ export function filterProducts(products) {
     }
 
     if (filterValue === "date-desc") {
+      sortedProducts = products.sort(
+        (a, b) => new Date(b.created_at) - new Date(a.created_at)
+      );
+    }
+
+    if (filterValue === "default") {
       sortedProducts = products.sort(
         (a, b) => new Date(b.created_at) - new Date(a.created_at)
       );
--- js/components/forms/searchProducts.js
+++ js/components/forms/searchProducts.js
@@ -2,18 +2,24 @@ import { renderProducts } from "../../ui/renderProducts.js";
 
 export function searchProducts(products) {
   const searchInput = document.querySelector("#search-input");
+  const searchIcon = document.querySelector("#search-icon");
+  const sortForm = document.querySelector("#sort-form");
+
+  searchIcon.onclick = function () {
+    renderProducts(products);
+    sortForm.reset();
+  };
 
   searchInput.onkeyup = function (event) {
+    sortForm.reset();
     const searchValue = event.target.value.trim().toLowerCase();
 
     const filteredProducts = products.filter(function (product) {
       if (
         product.title.toLowerCase().includes(searchValue) ||
         product.short_description.toLowerCase().includes(searchValue) ||
-        product.description.toLowerCase().includes(searchValue) ||
         product.title.toLowerCase().startsWith(searchValue) ||
-        product.short_description.toLowerCase().startsWith(searchValue) ||
-        product.description.toLowerCase().includes(searchValue)
+        product.short_description.toLowerCase().startsWith(searchValue)
       ) {
         return true;
       }
